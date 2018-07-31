dd5fde50a4ac12509703e37eb07399e25a12d4d1
xiaoyi254510255 <254510255@qq.com>
2018-07-31 10:33:16 +0800

    Create 21MergeTwoSortedLists.py
    21. 合并两个有序链表  21. Merge Two Sorted Lists

---
+++ 21MergeTwoSortedLists.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 == None or l2 == None:
+            return l2 if l1 == None else l1
+        
+        head = None
+        
+        if l1.val < l2.val:
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+        temp = head
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        temp.next = l2 if l1 == None else l1
+        
+        return head
